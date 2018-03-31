73a4b0d3f3855a49c67cd19441d1db4cce42dc7e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-03-31 17:39:03 -0300

    feat: added initial Fragment component

---
+++ src/Fragment.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import { QueryContext } from './Query'
+
+export const ERRORS = {
+  NO_PARENT_QUERY: new Error('Fragment component must be nested in a Query or Mutation component'),
+  NO_FRAGMENT_PROP: new Error('Fragment component must be provided with a fragment prop'),
+}
+
+class QueryFragment extends PureComponent {
+  static propTypes = {
+    id: PropTypes.string, // Optional fragment cache id.
+    queryContext: PropTypes.any.isRequired,
+    fragment: PropTypes.object.isRequired, // AST proptype?
+    children: PropTypes.func.isRequired,
+  }
+
+  constructor (...args) {
+    super(...args)
+
+    if (!this.props.queryContext) throw ERRORS.NO_PARENT_QUERY
+    if (!this.props.fragment) throw ERRORS.NO_FRAGMENT_PROP
+
+    // Register fragment:
+    // @TODO: it this fragment found on this query?
+    this.props.queryContext.registerFragment(this.props.fragment)
+  }
+
+  render () {
+    const { children, fragment, id, queryContext } = this.props
+
+    const result = queryContext.getFragmentResult({ id, fragment })
+
+    return children(result)
+  }
+}
+
+export const Fragment = props => (
+  <QueryContext.Consumer>
+    { queryContext => (
+      <QueryFragment
+        { ...props }
+        queryContext={ queryContext }
+      />
+    ) }
+  </QueryContext.Consumer>
+)
---
+++ tests/Fragment.test.js
@@ -0,0 +1,181 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { concatAST } from 'graphql'
+import { default as originalGql } from 'graphql-tag'
+import { addTypenameToDocument } from 'apollo-utilities'
+import { MockedProvider } from '../local_modules/react-apollo/test-utils'
+
+import { Fragment, Query } from 'react-apollo-defragment'
+import { ERRORS } from 'react-apollo-defragment/Fragment'
+
+const filters = {}
+const logTypes = ['log', 'info', 'warn', 'error']
+
+logTypes.forEach(type => {
+  filters[type] = []
+
+  const original = console[type]
+
+  console[type] = (...args) => {
+    const text = args[0] && args[0].toString && args[0].toString()
+    if (!filters.error.some(filter => filter.test(text))) {
+      original(...args)
+    }
+  }
+
+  console[type].filter = regexes => {
+    filters[type] = filters[type].concat(regexes)
+  }
+
+  console[type].clearFilters = () => {
+    filters[type] = []
+  }
+})
+
+const sleep = (ms = 1) => new Promise(resolve => setTimeout(resolve, ms))
+
+const gql = (...args) => addTypenameToDocument(originalGql(...args))
+
+describe.only('Fragment', () => {
+  // Testing children render prop implementations.
+  const childrens = {}
+  childrens.nil = jest.fn(() => null)
+  childrens.div = jest.fn(() => <div>content</div>)
+
+  // Testing fragment documents.
+  const fragment = gql`fragment NamedOnType on Type { field }`
+
+  // Testing query documents.
+  const queries = {
+    simple: gql`query Simple { typeResolver { id ...NamedOnType } }`
+  }
+
+  const defragmentedQueries = {
+    simple: concatAST([queries.simple, fragment])
+  }
+
+  const mocks = {
+    simple: [{
+      request: { query: defragmentedQueries.simple },
+      result: { data: { typeResolver: { id: '1', field: 'value', __typename: 'Type' } } }
+    }]
+  }
+
+  const wrappedListener = jest.fn()
+
+  const wrapInQuery = element => (
+    <MockedProvider mocks={ mocks.simple } removeTypename>
+      <Query query={ queries.simple }>
+        { wrappedListener.mockReturnValue(element) }
+      </Query>
+    </MockedProvider>
+  )
+
+  beforeEach(jest.clearAllMocks)
+  beforeEach(console.error.clearFilters)
+
+  it('should render children', () => {
+    const wrapper = mount(wrapInQuery(
+      <Fragment fragment={ fragment }>{ childrens.div }</Fragment>
+    ))
+
+    expect(childrens.div).toHaveBeenCalled()
+    expect(wrapper.html()).toBe('<div>content</div>')
+  })
+
+  it('should throw when no fragment is provided', () => {
+    console.error.filter([
+      /The above error occurred in the/,
+      new RegExp(ERRORS.NO_FRAGMENT_PROP),
+      /The prop `fragment` is marked as required/
+    ])
+
+    expect(() => mount(wrapInQuery(<Fragment>{ childrens.nil }</Fragment>)))
+      .toThrow(ERRORS.NO_FRAGMENT_PROP)
+
+    expect(childrens.nil).not.toHaveBeenCalled()
+  })
+
+  it('should throw when not nested in a Query/Mutation component', () => {
+    console.error.filter([
+      /The above error occurred in the/,
+      new RegExp(ERRORS.NO_PARENT_QUERY),
+      /The prop `query` is marked as required/
+    ])
+
+    expect(() => mount(<Fragment fragment={ fragment }>{ childrens.nil }</Fragment>))
+      .toThrow(ERRORS.NO_PARENT_QUERY)
+  })
+
+  it('should automatically add fragments to a parent query', async () => {
+    const wrapper = mount(wrapInQuery(
+      <Fragment fragment={ fragment }>{ childrens.nil }</Fragment>
+    ))
+
+    await sleep()
+    wrapper.update()
+
+    expect(wrappedListener.mock).toHaveProperty('calls.0.0.loading', true)
+    expect(wrappedListener.mock).toHaveProperty('calls.1.0.loading', true)
+    expect(wrappedListener.mock).toHaveProperty('calls.2.0.loading', false)
+    expect(wrappedListener.mock).toHaveProperty('calls.2.0.data.typeResolver.field', 'value')
+  })
+
+  it('should provide Fragment children with query result object', async () => {
+    const wrapper = mount(wrapInQuery(
+      <Fragment fragment={ fragment }>{ childrens.nil }</Fragment>
+    ))
+
+    await sleep()
+    wrapper.update()
+
+    expect(childrens.nil.mock).toHaveProperty('calls.0.0.loading', true)
+    expect(childrens.nil.mock).toHaveProperty('calls.1.0.loading', true)
+    expect(childrens.nil.mock).toHaveProperty('calls.2.0.loading', false)
+
+    const resultKeys = [
+      'variables',
+      'refetch',
+      'fetchMore',
+      'updateQuery',
+      'startPolling',
+      'stopPolling',
+      'subscribeToMore',
+      'loading',
+      'networkStatus',
+      'error',
+      'client',
+    ]
+
+    resultKeys.forEach(key => {
+      expect(childrens.nil.mock).toHaveProperty(`calls.2.0.${key}`)
+    })
+  })
+
+  it('should provide Fragment children with fragment result data when id is available', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={ mocks.simple } removeTypename>
+        <Query query={ queries.simple }>
+          { ({ data, client }) => {
+            const id = data.typeResolver && client.cache.config.dataIdFromObject(
+              data.typeResolver
+            )
+
+            return (
+              <Fragment fragment={ fragment } id={ id }>
+                { childrens.nil }
+              </Fragment>
+            )
+          } }
+        </Query>
+      </MockedProvider>
+    )
+
+    await sleep()
+    wrapper.update()
+
+    expect(childrens.nil.mock).toHaveProperty('calls.2.0.data.field', 'value')
+  })
+
+  it('should provide Fragment children with fragment optimistic result data')
+})
