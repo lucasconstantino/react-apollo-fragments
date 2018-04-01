f63d0ea7ae013d2fb7c3c08d75542b2e55b081d4
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-04-01 19:50:51 -0300

    chore: removed deprecated api

--- src/utils.js
+++ src/utils.js
@@ -1,38 +1,6 @@
-import walk from 'react-tree-walker'
 import { visit, BREAK } from 'graphql'
 
 export const unique = (v, i, arr) => arr.indexOf(v) === i
-
-/**
- * Asynchronously walk a React component tree to extract
- * any encountered fragments (via static properties on components).
- *
- * @param {Object} tree A React component tree.
- * @param {Function} dive Predicate function to decide if should dive deeper.
- * @param {Object} dive.fragment The extracted fragment.
- * @param dive[...] The walking parameters.
- *
- * @return {Array} extracted fragments.
- */
-export const getFragmentsFromTree = async (tree, dive = () => true) => {
-  const fragments = []
-
-  if (tree) {
-    await walk(tree, (element, ...args) => {
-      const fragment = element.type && element.type.fragment
-        ? element.type.fragment
-        : null
-
-      const diving = dive(fragment, element, ...args)
-
-      if (fragment && diving) fragments.push(fragment)
-
-      return diving
-    })
-  }
-
-  return fragments
-}
 
 /**
  * Given an AST, extract the first available fragment name.
@@ -91,36 +59,4 @@ export const getRequestedFragmentNames = ast => {
   })
 
   return fragmentNames
-}
-
-/**
- * Asynchronously walk a React component tree to extract
- * any encountered fragments present on the given AST.
- *
- * @param {Object} tree A React component tree.
- * @param {Object} ast GraphQL AST.
- *
- * @return {Array} extracted fragments.
- */
-export const getQueryFragmentsFromTree = (tree, ast) => {
-  let requestedFragments = getRequestedFragmentNames(ast)
-
-  return getFragmentsFromTree(tree, fragment => {
-    // When a fragment is found, eiher:
-    // 1. it was requested by the AST, so not only should we extract this
-    //    fragment, but look for any nested fragments inside it.
-    // 2. it was not requested by the AST, meaning we should skip it altogether.
-    if (fragment) {
-      if (requestedFragments.indexOf(getFragmentName(fragment)) !== -1) {
-        requestedFragments = requestedFragments.concat(getRequestedFragmentNames(fragment))
-        return true
-      }
-      else {
-        return false
-      }
-    }
-
-    // When no fragment found, just keep digging.
-    return true
-  })
 }
